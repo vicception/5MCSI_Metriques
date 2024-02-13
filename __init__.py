4c0afe9cf1e5806282f98c772a2604e371a1dceb
vicception <127982347+vicception@users.noreply.github.com>
2024-02-13 14:48:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
+
+@app.route("/commits/")
+def mescommits():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
