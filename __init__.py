d7e45ef4d22497f7574a6bbff972637232a2d002
vicception <127982347+vicception@users.noreply.github.com>
2024-02-13 14:32:28 +0100

    Update __init__.py
    Moved rapport-bis to histogramme

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/rapport-bis/")
+@app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
   
