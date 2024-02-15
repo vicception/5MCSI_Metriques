5308534def9fc53a27e9edee1beb7f9ed38671b5
vicception <127982347+vicception@users.noreply.github.com>
2024-02-15 09:12:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
