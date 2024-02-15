619df52522ddb97ff583fb129eef4279db9bb2c8
vicception <127982347+vicception@users.noreply.github.com>
2024-02-15 09:22:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ def hello_world():
     return render_template('hello.html')
 
 @app.route("/contact/")
-def MaPremiereAPI():
+def contact():
     return render_template("contact.html")
 
 @app.route('/tawarano/')
