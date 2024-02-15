c97e10ba4af64d6d43db9f706888f91858eb3ed0
vicception <127982347+vicception@users.noreply.github.com>
2024-02-15 09:25:17 +0100

    Update __init__.py
    Added CSRF protection

--- __init__.py
+++ __init__.py
@@ -5,7 +5,9 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
                                                                                                                                        
 @app.errorhandler(404)
 def page_not_found(e):
